32f5d5129662a394f8672120e6b691000f5744c9
Henric Trotzig <henric.trotzig@gmail.com>
2020-05-15 10:10:40 +0200

    Replace external API with a mock DB
    The external API might go down and I don't want that to happen while I'm
    doing the Cypress + Happo demo. Replacing it with a hard-coded mock db
    (sourced from the real one) will make things more predictable.

--- src/js/toolbox/components/api/index.js
+++ src/js/toolbox/components/api/index.js
@@ -1,60 +1,57 @@
+import mockDB from './mockDB';
+
+function findTracksByAlbumName(name) {
+  // [{"name":"01 Cycle","artist":"Beck","album":"Morning Phase","track":1,"url":"files\/music\/Beck\/Morning Phase\/01 Cycle.mp3","artwork":"files\/music\/Beck\/Morning Phase\/Artwork.png"}];
+  for (const artist of mockDB) {
+    for (const album of artist.albums || []) {
+      if (album.album === name) {
+        return album.tracks.map(track => ({
+          name: track.name,
+          artist: artist.artist,
+          album: album.album,
+          track: track.track,
+          url: track.url,
+          artwork: album.artwork,
+        }));
+      }
+    }
+  }
+  return [];
+}
 class ApiClass {
-  constructor() {
-    this.url = `https://57c62f9e.ngrok.io/api`;
+  fetchArtists() {
+    return Promise.resolve(mockDB.map(({ artist }) => ({ artist })));
   }
 
-  makeRequest = ({ url, data }) => {
-    return new Promise(function(resolve, reject) {
-      fetch(url, data)
-        .then(response => {
-          response.json().then(data => {
-            if (response.status >= 300) {
-              reject(data.message);
-            }
-            resolve(data);
-          });
-        })
-        .catch(e => {
-          reject(Error(e));
+  fetchArtist(artistName) {
+    // [{"album":"Morning Phase","artist":"Beck","artwork":"files\/music\/Beck\/Morning Phase\/Artwork.png"}]
+    const artist = mockDB.find(a => a.artist === artistName);
+    const result = artist.albums.map(album => ({
+      album: album.album,
+      artist: artist.artist,
+      artwork: album.artwork,
+    }));
+    return Promise.resolve(result);
+  }
+
+  fetchAlbums() {
+    const albums = [];
+    mockDB.forEach(artist => {
+      (artist.albums || []).forEach(album => {
+        albums.push({
+          album: album.album,
+          artist: artist.artist,
+          artwork: album.artwork,
         });
+      });
     });
-  };
+    return Promise.resolve(albums);
+  }
 
-  fetchArtists = () => {
-    return this.makeRequest({
-      url: `${this.url}/artists`,
-      data: {
-        method: "GET"
-      }
-    });
-  };
-
-  fetchArtist = artist => {
-    return this.makeRequest({
-      url: `${this.url}/artist/${artist}`,
-      data: {
-        method: "GET"
-      }
-    });
-  };
-
-  fetchAlbums = () => {
-    return this.makeRequest({
-      url: `${this.url}/albums`,
-      data: {
-        method: "GET"
-      }
-    });
-  };
-
-  fetchAlbum = ({ album }) => {
-    return this.makeRequest({
-      url: `${this.url}/album/${album}`,
-      data: {
-        method: "GET"
-      }
-    });
-  };
+  fetchAlbum({ album }) {
+    // [{"name":"01 Cycle","artist":"Beck","album":"Morning Phase","track":1,"url":"files\/music\/Beck\/Morning Phase\/01 Cycle.mp3","artwork":"files\/music\/Beck\/Morning Phase\/Artwork.png"}];
+    return Promise.resolve(findTracksByAlbumName(album));
+  }
 }
 
 const Api = new ApiClass();
---
+++ src/js/toolbox/components/api/mockDB.js
@@ -0,0 +1,183 @@
+export default [
+  { artist: 'Active Child' },
+  { artist: 'Andrew Bird' },
+  { artist: 'Bastille' },
+  { artist: 'Bay Ledges' },
+  {
+    artist: 'Beck',
+    albums: [
+      {
+        album: 'Morning Phase',
+        artwork: 'files/music/Beck/Morning Phase/Artwork.png',
+        tracks: [
+          {
+            name: '01 Cycle',
+            track: 1,
+            url: 'files/music/Beck/Morning Phase/01 Cycle.mp3',
+          },
+          {
+            name: '02 Morning',
+            track: 2,
+            url: 'files/music/Beck/Morning Phase/02 Morning.mp3',
+          },
+          {
+            name: '03 Heart Is a Drum',
+            track: 3,
+            url: 'files/music/Beck/Morning Phase/03 Heart Is a Drum.mp3',
+          },
+          {
+            name: '04 Say Goodbye',
+            track: 4,
+            url: 'files/music/Beck/Morning Phase/04 Say Goodbye.mp3',
+          },
+          {
+            name: '05 Blue Moon',
+            track: 5,
+            url: 'files/music/Beck/Morning Phase/05 Blue Moon.mp3',
+          },
+          {
+            name: '06 Unforgiven',
+            track: 6,
+            url: 'files/music/Beck/Morning Phase/06 Unforgiven.mp3',
+          },
+          {
+            name: '07 Wave',
+            track: 7,
+            url: 'files/music/Beck/Morning Phase/07 Wave.mp3',
+          },
+          {
+            name: '08 Dont Let It Go',
+            track: 8,
+            url: 'files/music/Beck/Morning Phase/08 Dont Let It Go.mp3',
+          },
+          {
+            name: '09 Blackbird Chain',
+            track: 9,
+            url: 'files/music/Beck/Morning Phase/09 Blackbird Chain.mp3',
+          },
+          {
+            name: '10 Phase',
+            track: 10,
+            url: 'files/music/Beck/Morning Phase/10 Phase.mp3',
+          },
+          {
+            name: '11 Turn Away',
+            track: 11,
+            url: 'files/music/Beck/Morning Phase/11 Turn Away.mp3',
+          },
+          {
+            name: '12 Country Down',
+            track: 12,
+            url: 'files/music/Beck/Morning Phase/12 Country Down.mp3',
+          },
+          {
+            name: '13 Waking Light',
+            track: 13,
+            url: 'files/music/Beck/Morning Phase/13 Waking Light.mp3',
+          },
+        ],
+      },
+    ],
+  },
+  { artist: 'Bon Iver' },
+  { artist: 'Capital Cities' },
+  { artist: 'City of the Sun' },
+  { artist: 'Clean Bandit' },
+  {
+    artist: 'Coldplay',
+    albums: [
+      {
+        album: 'A Head Full of Dreams',
+        artwork: 'files/music/Coldplay/A Head Full of Dreams/Artwork.png',
+        tracks: [
+          {
+            name: '01 A Head Full of Dreams',
+            track: 1,
+            url:
+              'files/music/Coldplay/A Head Full of Dreams/01 A Head Full of Dreams.m4a',
+          },
+          {
+            name: '02 Birds',
+            track: 2,
+            url: 'files/music/Coldplay/A Head Full of Dreams/02 Birds.m4a',
+          },
+          {
+            name: '03 Hymn for the Weekend',
+            track: 3,
+            url:
+              'files/music/Coldplay/A Head Full of Dreams/03 Hymn for the Weekend.m4a',
+          },
+        ],
+      },
+      {
+        album: 'A Rush Of Blood To The Head',
+        artwork: 'files/music/Coldplay/A Rush Of Blood To The Head/Artwork.png',
+        tracks: [
+          {
+            name: '01 Politik',
+            track: 1,
+            url:
+              'files/music/Coldplay/A Rush Of Blood To The Head/01 Politik.m4a',
+          },
+          {
+            name: '02 In My Place',
+            track: 2,
+            url:
+              'files/music/Coldplay/A Rush Of Blood To The Head/02 In My Place.m4a',
+          },
+          {
+            name: '03 God Put A Smile Upon Your Face',
+            track: 3,
+            url:
+              'files/music/Coldplay/A Rush Of Blood To The Head/03 God Put A Smile Upon Your Face.m4a',
+          },
+          {
+            name: '04 The Scientist',
+            track: 4,
+            url:
+              'files/music/Coldplay/A Rush Of Blood To The Head/04 The Scientist.m4a',
+          },
+          {
+            name: '05 Clocks',
+            track: 5,
+            url:
+              'files/music/Coldplay/A Rush Of Blood To The Head/05 Clocks.m4a',
+          },
+          {
+            name: '06 Daylight',
+            track: 6,
+            url:
+              'files/music/Coldplay/A Rush Of Blood To The Head/06 Daylight.m4a',
+          },
+        ],
+      },
+    ],
+  },
+  { artist: 'Courtship.' },
+  { artist: 'Exmag' },
+  { artist: 'Foals' },
+  { artist: 'Foster the People' },
+  { artist: 'Frank Ocean' },
+  { artist: 'Half Moon Run' },
+  { artist: 'Jose Gonzalez' },
+  { artist: 'Justin Hurwitz' },
+  { artist: 'Kygo' },
+  { artist: 'Lauv' },
+  { artist: 'M83' },
+  { artist: 'Monstercat' },
+  { artist: 'ODESZA' },
+  { artist: 'Portugal. The Man' },
+  { artist: 'Post Malone' },
+  { artist: 'POWERS' },
+  { artist: 'Real Estate' },
+  { artist: 'Roosevelt' },
+  { artist: 'St Lucia' },
+  { artist: 'Taylor Swift' },
+  { artist: 'Tears For Fears' },
+  { artist: 'The Knocks' },
+  { artist: 'Tomggg' },
+  { artist: 'Tycho' },
+  { artist: 'Vicetone' },
+  { artist: 'Volcano Choir' },
+  { artist: 'Who Is Fancy' },
+];
