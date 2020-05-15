95855dab5b8d2d7fa7ea69c99f61c46928d68022
Henric Trotzig <henric.trotzig@gmail.com>
2020-05-15 11:03:14 +0200

    Add happo integration
    Following the guide here:
    https://docs.happo.io/docs/cypress

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,13 +1,45 @@
 describe('apple-music-js app', () => {
   it('works', () => {
     cy.visit('/');
+    cy.get('[data-test="welcome-closing"]');
+    cy.get('body').happoScreenshot({ component: 'Loading screen' });
+
+    cy.get('[data-test="welcome-closing"]').should('not.be.visible');
+
+    cy.get('body').happoScreenshot({ component: 'Main screen' });
+
     cy.contains('Artists').click();
+
+    cy.contains('Coldplay');
+    cy.get('body').happoScreenshot({ component: 'Artists screen' });
+
     cy.contains('Coldplay').click({ force: true });
+
+    cy.get('[data-test="album-button"]').happoScreenshot({
+      component: 'Album button',
+    });
+
     cy.contains('A Head Full of Dreams').click();
+
+    cy.get('[data-test="mini-controls"]').happoScreenshot({
+      component: 'Mini controls',
+      variant: 'no track selected',
+    });
+
     cy.contains('Hymn for the Weekend').click();
 
     cy.get('[data-test="mini-pause"]').click();
 
+    cy.get('[data-test="mini-controls"]').happoScreenshot({
+      component: 'Mini controls',
+      variant: 'with track selected',
+    });
+
     cy.get('[data-test="mini-controls"]').click();
+
+    cy.get('[data-test="controls"]').happoScreenshot({
+      component: 'Controls',
+    });
+
   });
 });
